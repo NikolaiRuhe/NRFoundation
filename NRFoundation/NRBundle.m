6c93259d59911779456cd5c22d0f9de3482a3922
Nikolai Ruhe <nikolai@flightrecorderapp.com>
2013-06-04 16:01:31 +0200

    Fixed a compiler warning.

--- NRFoundation/NRBundle.m
+++ NRFoundation/NRBundle.m
@@ -20,21 +20,24 @@
 	if (execute_header == NULL)
 		return @"unknown";
 
-	const uint8_t *command = (const uint8_t *)(execute_header + 1);
+	union {
+		const uint8_t *ptr;
+		const struct load_command *com;
+	} command;
+	command.ptr = (const void *)(execute_header + 1);
 
 	for (uint32_t idx = 0; idx < execute_header->ncmds; ++idx) {
-		if (((const struct load_command *)command)->cmd == LC_UUID) {
-			command += sizeof(struct load_command);
+		if (command.com->cmd == LC_UUID) {
+			command.com += 1;
 
 			return [NSString stringWithFormat:@"%02X%02X%02X%02X-%02X%02X-%02X%02X-%02X%02X-%02X%02X%02X%02X%02X%02X",
-					command[0], command[1], command[2], command[3],
-					command[4], command[5],
-					command[6], command[7],
-					command[8], command[9],
-					command[10], command[11], command[12], command[13], command[14], command[15]];
-		} else {
-			command += ((const struct load_command *)command)->cmdsize;
+					command.ptr[0], command.ptr[1], command.ptr[2], command.ptr[3],
+					command.ptr[4], command.ptr[5],
+					command.ptr[6], command.ptr[7],
+					command.ptr[8], command.ptr[9],
+					command.ptr[10], command.ptr[11], command.ptr[12], command.ptr[13], command.ptr[14], command.ptr[15]];
 		}
+		command.ptr += command.com->cmdsize;
 	}
 
 	return @"unknown";
