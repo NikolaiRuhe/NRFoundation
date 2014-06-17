e8544c4385c8204369bbfea456c1647b1612be5f
Nikolai Ruhe <git@nikolai-ruhe.de>
2014-06-17 13:16:14 +0200

    Fix a bug on arm 64 bit in delayed target mechanism.

--- NRFoundation/NRObject.m
+++ NRFoundation/NRObject.m
@@ -134,8 +134,8 @@ static void NRPerformOnDelayedTargetsQueue(void(^block)())
 
 static void NRDelayedTargetsSetApplierFunction(const void *target, void *selector)
 {
-	typedef id (*plainMessageSendNoArgs)(id, SEL);
-	((plainMessageSendNoArgs)objc_msgSend)((__bridge id)target, selector);
+	typedef void (* plainMessageSendNoArgs)(const void *target, void *selector);
+	((plainMessageSendNoArgs)objc_msgSend)(target, selector);
 }
 
 static void NRDelayedTargetsDictionaryApplierFunction(const void *key, const void *value, void *context)
